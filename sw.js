58becbfaf67f4439aa22ff1c6784d6e2d1c26074
araperu87-sudo <araperu87@gmail.com>
2026-05-16 18:45:23 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+// Service Worker Básico para habilitar la instalación PWA
+const CACHE_NAME = 'ecocore-cache-v1';
+const assets = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
